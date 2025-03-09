c983633a4a4c0710f1b776083e51745519cc48ba
Nishit Lugun <lugun.nishit@gmail.com>
2025-03-10 02:23:28 +0530

    render problem

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,4 +1,3 @@
-// src/context/SocketContext.js
 "use client";
 import { createContext, useContext, useEffect, useState } from "react";
 import io from "socket.io-client";
@@ -11,6 +10,7 @@ export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
+    // Use the NEXT_PUBLIC_SERVER_URL environment variable
     const newSocket = io(
       process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000"
     );
